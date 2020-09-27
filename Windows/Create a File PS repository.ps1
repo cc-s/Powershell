ee484ed00b4c5e77b4e65677f19340e6b4673ce1
cc <cc>
2020-09-27 16:37:22 +0200

    Create PS File Repository

---
+++ Windows/Create a File PS repository.ps1
@@ -0,0 +1,16 @@
+#Create a File PS repository
+
+# Register a file share on my local machine
+$Share = "\\localhost\PSRepository"
+$Name = "CC-Repository"
+Register-PSRepository -Name $Name-SourceLocation $Share -ScriptSourceLocation $Share -InstallationPolicy Trusted
+
+Get-PSRepository
+
+# Publish to a file share repo - the NuGet API key must be a non-blank string
+$ModulePath = "E:\Users\Carl C\Documents\WindowsPowerShell\Modules\NovelScripts"
+Publish-Module -Path $ModulePath -Repository "$Name-SourceLocation" -NuGetApiKey 'AnyStringWillDo'
+
+Find-Module -Repository CC-Repository-SourceLocation
+
+Install-Module -Repository Repository-SourceLocation -Name NovelScripts -Scope AllUsers
