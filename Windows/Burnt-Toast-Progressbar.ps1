13e47cadcce46733fff2729b110aecc1b9e9b808
cc <cc>
2022-02-20 22:30:40 +0100

    Added comments

--- Windows/Burnt-Toast-Progressbar.ps1
+++ Windows/Burnt-Toast-Progressbar.ps1
@@ -1,25 +1,31 @@
 $Files = Get-ChildItem "C:\Windows\System32"
 
-$ToastHeader = New-BTHeader -Id '001' -Title "Toast Rubrik" -Arguments 'http://google.se"'
-$Progress = New-BTProgressBar -Status 'Copying files' -Indeterminate
+#Create the initial toast message
+$Status = "Copying files"
+$Text = "Something is happening"
+$id = "msg001"
+#Argument opens a file
+$ToastHeader = New-BTHeader -Id $id -Title "Toast title (click)" -Arguments "C:\temp\log.log"
+$Progress = New-BTProgressBar -Status $Status -Indeterminate
 
+New-BurntToastNotification -Text $Text `
+     -ProgressBar $Progress -Header $ToastHeader -UniqueIdentifier $id
 
-New-BurntToastNotification -Text 'File copy script running', 'Hmm!' `
-     -ProgressBar $Progress -Header $ToastHeader -UniqueIdentifier 'msg001'
+# Calculate a 25% update interval
+[int]$Interval = ($Files.count) * 0.25
 
-[int]$Intervall = ($Files.count) * 0.25
-
+#Counter to determine progress update
 $i = 1
-foreach ($file in $Files) {
+
+foreach ($File in $Files) {
      $i += 1
-     #  $Intervall = [Math]::Round($i/$Files.count,1)
-
-     if ($i -eq $Intervall -or $i -eq $($Intervall * 2) -or $i -eq $($Intervall * 3) -or $i -eq $($Intervall * 4)) {
+     if ($i -eq $Interval -or $i -eq $($Interval * 2) -or $i -eq $($Interval * 3) -or $i -eq $($Interval * 4)) {
           $per = [Math]::Round($i / ($Files.count), 2)
-          $Progress = New-BTProgressBar -Status 'This is not happening' -Value $per
-          New-BurntToastNotification -Text "Deleting files", 'Hmm!' `
-               -ProgressBar $Progress -Header $ToastHeader -UniqueIdentifier 'msg001'
-     }#end if
+          $Progress = New-BTProgressBar -Status $Status -Value $per
+          New-BurntToastNotification -Text $Text `
+               -ProgressBar $Progress -Header $ToastHeader -UniqueIdentifier $id
+     }#End if
 
+     #What to do
      Start-Sleep -Milliseconds 10
-}
+}#End foreach File
