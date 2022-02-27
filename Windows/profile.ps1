bfde0c37b136f0ec9b5698d98b9dbb57b5cc161d
cc <cc>
2022-02-27 23:35:21 +0100

    Added Powershell 7 Profile settings

---
+++ Windows/profile.ps1
@@ -0,0 +1,11 @@
+#Powershell 7 Profile
+#Enable-ExperimentalFeature PSAnsiRenderingFileInfo
+#$PSStyle.Reset
+$PSStyle.FileInfo.Extension.item(".ps1") = "`e[38;5;69m"
+$PSStyle.FileInfo.extension.add(".txt", "`e[33;1m")
+$PSStyle.FileInfo.extension.add(".log", "`e[33;1m")
+$PSStyle.FileInfo.extension.add(".csv", "`e[95m")
+$PSStyle.FileInfo.extension.add(".xml", "`e[95m")
+$PSStyle.FileInfo.extension.add(".json", "`e[95m")
+
+function Profile { code $PROFILE.CurrentUserAllHosts }
