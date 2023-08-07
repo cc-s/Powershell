ef63d4d0acb358e2470fc83171172501caa85c28
CC <ccs.135@gmail.com>
2023-08-07 22:01:44 +0200

    Added diskperf counter

--- Windows/Get-WindowsInventory.ps1
+++ Windows/Get-WindowsInventory.ps1
@@ -106,8 +106,6 @@ function Get-CPUInfo {
 }#Get-CPUInfo
 
 
-#$CPUPerformance = (Get-Counter "\Processor(_total)\% Processor Time" -SampleInterval 30 -MaxSamples 1).CounterSamples.CookedValue
-
 #Memory
 
 Function Get-MemoryInfo {
@@ -223,6 +221,190 @@ function Get-PartitionInfo {
      
 }#Get-PartitionInfo
 
+
+function Get-CPUPerformance {
+
+     $CPUPerformance = (Get-Counter "\Processor(_total)\% Processor Time" -SampleInterval 30 -MaxSamples 1).CounterSamples.CookedValue
+}
+
+
+
+
+#Diskperformace Guide
+<#
+IO Disk Transfers/sec (Disk Reads/sec, Disk Writes/sec)
+
+Latancy Avg. Disk sec/Transfer (Avg. Disk sec/Read, Avg. Disk sec/Write) 
+
+IO Size Avg. Disk Bytes/Transfer (Avg. Disk Bytes/Read, Avg. Disk Bytes/Write) 
+
+Throughput Disk Bytes/sec (Disk Read Bytes/sec, Disk Write Bytes/sec) 
+#>
+
+function Get-IOPS {
+
+
+     [CmdletBinding()]
+     param
+     (
+          $Drive,
+
+          $SampleInterval,
+
+          $MaxSamples
+     )
+     $Obj = @()
+     #Swedish
+     #$IOs = Get-Counter -Counter "\Logisk disk($Drive)\Disköverföringar/s" -SampleInterval $SampleInterval -MaxSamples $MaxSamples | select -ExpandProperty CounterSamples
+  
+     $IOs = Get-Counter -Counter "\LogicalDisk($Drive)\Disk Transfers/sec" -SampleInterval $SampleInterval -MaxSamples $MaxSamples | Select-Object -ExpandProperty CounterSamples
+  
+     foreach ($value in $IOs) {
+
+          $Props = [ordered]@{
+               Disk      = $value.InstanceName
+               IOPS      = $value.CookedValue -as [int]
+               Timestamp = $value.Timestamp
+
+          }
+
+          $Obj += New-Object -TypeName psobject -Property $Props
+     }
+
+     $Obj | Sort-Object -Property disk
+
+} #End Function IOPS
+
+function Get-Latency {
+
+
+     [CmdletBinding()]
+     param
+     (
+          $Drive,
+
+          $SampleInterval,
+
+          $MaxSamples
+     )
+     $Obj = @()
+     #Swedish
+     #$Counters = Get-Counter -Counter "\Logisk disk($Drive)\Medel s/disköverföring" -SampleInterval $SampleInterval -MaxSamples $MaxSamples | select -ExpandProperty CounterSamples
+
+     $Counters = Get-Counter -Counter "\LogicalDisk($Drive)\Avg. Disk sec/Transfer" -SampleInterval $SampleInterval -MaxSamples $MaxSamples | Select-Object -ExpandProperty CounterSamples
+
+     foreach ($value in $Counters) {
+
+          $Props = [ordered]@{
+               Disk      = $value.InstanceName
+               Latency   = $value.CookedValue -as [int]
+               Timestamp = $value.Timestamp
+
+          }
+
+          $Obj += New-Object -TypeName psobject -Property $Props
+     }
+
+     $Obj | Sort-Object disk
+
+} #End Function Latency
+
+function Get-DiskPerformance {
+
+     [CmdletBinding()]
+     param
+     (
+          $Drive,
+
+          $SampleInterval,
+
+          $MaxSamples
+     )
+  
+     $Obj = @()
+     #Swedish
+     #$Counters = Get-Counter -Counter "\Logisk disk($Drive)\Medel s/disköverföring" -SampleInterval $SampleInterval -MaxSamples $MaxSamples | select -ExpandProperty CounterSamples
+
+     $Counters = Get-Counter -Counter "\LogicalDisk($Drive)\Disk Bytes/sec" -SampleInterval $SampleInterval -MaxSamples $MaxSamples | Select-Object -ExpandProperty CounterSamples
+
+     foreach ($value in $Counters) {
+
+          $Props = [ordered]@{
+               Disk       = $value.InstanceName
+               Throughput = $value.CookedValue -as [int]
+               Timestamp  = $value.Timestamp
+          }
+
+          $Obj += New-Object -TypeName psobject -Property $Props
+     }
+
+     $Obj | Sort-Object disk
+
+} #End Function DiskPerformance
+
+
+#IO
+function Get-DiskPerformanceInfo {
+     [CmdletBinding()]
+     param
+     (
+          $Drive,
+          $SampleInterval,
+          $MaxSamples
+     )
+     $Obj = @()
+     
+     
+     $Counters = "\LogicalDisk($Drive)\Disk Transfers/sec", "\LogicalDisk($Drive)\Avg. Disk sec/Transfer", "\LogicalDisk($Drive)\Disk Bytes/sec"
+
+     $DiskPerf = Get-Counter -Counter $Counters -SampleInterval $SampleInterval -MaxSamples $MaxSamples | Select-Object -ExpandProperty CounterSamples
+
+     $Props = [ordered]@{
+          Disk         = ($DiskPerf | Where-Object path -Like "*disk transfers*").InstanceName
+          IOPS         = ($DiskPerf | Where-Object path -Like "*disk transfers*").CookedValue -as [int]
+          LatencyMS    = ($DiskPerf | Where-Object path -Like "*Avg*").CookedValue -as [int]
+          ThroughputKB = (($DiskPerf | Where-Object path -Like "*Disk Bytes*").CookedValue / 1kb) -as [int]
+          Timestamp    = $DiskPerf[0].Timestamp
+     }
+
+     $Obj += New-Object -TypeName psobject -Property $Props
+
+     $Obj | Sort-Object -Property disk
+
+} #End Function Get-DiskPerformanceInfo
+
+$Drives = (Get-VolumeInfo | Where-Object DriveLetter -NE $null | Where-Object DriveType -EQ "Fixed").DriveLetter
+$SampleInterval = 20
+$MaxSamples = 1
+
+Get-DiskPerformanceInfo -Drive "$($Drives):" -SampleInterval $SampleInterval -MaxSamples $MaxSamples
+
+function Get-old {
+     $Drives = (Get-VolumeInfo | Where-Object DriveLetter -NE $null | Where-Object DriveType -EQ "Fixed").DriveLetter
+     $SampleInterval = 2
+     $MaxSamples = 1
+     
+     $IOps = @()
+     $Latency = @()
+     $Throughput = @()
+     foreach ($Drive in $Drives) {
+     
+          $IOps += Get-IOPS -Drive "$($Drive):" -SampleInterval $SampleInterval -MaxSamples $MaxSamples
+          $Latency += Get-Latency -Drive "$($Drive):" -SampleInterval $SampleInterval -MaxSamples $MaxSamples
+          $Throughput += Get-DiskPerformance -Drive "$($Drive):" -SampleInterval $SampleInterval -MaxSamples $MaxSamples
+     }#End Drives
+
+
+     
+}#Get-DiskPerformanceInfo
+
+
+
+
+
+
+
+
 Get-SystemInfo | Format-Table
 Get-OSInfo | Format-Table
 Get-CPUInfo | Format-Table
@@ -230,3 +412,85 @@ Get-MemoryInfo | Format-Table
 Get-DiskInfo | Format-Table
 Get-VolumeInfo | Format-Table
 Get-PartitionInfo | Format-Table
+
+$RPSystem = Get-SystemInfo | ConvertTo-Html -As List -Fragment -PreContent "<h2>System Information</h2>"
+$RPOS = Get-OSInfo | ConvertTo-Html -As List -Fragment -PreContent "<h2>OS Information</h2>"
+$RPCPU = Get-CPUInfo | ConvertTo-Html -As List -Fragment -PreContent "<h2>Processor Information</h2>"
+$RPMemory = Get-MemoryInfo | ConvertTo-Html -As List -Fragment -PreContent "<h2>Memory Information</h2>"
+$RPDisk = Get-DiskInfo | ConvertTo-Html -As Table -Fragment -PreContent "<h2>Disk Information</h2>"
+$RPVolume = Get-VolumeInfo | ConvertTo-Html -As Table -Fragment -PreContent "<h2>Volume Information</h2>"
+$RPPartition = Get-PartitionInfo | ConvertTo-Html -As Table -Fragment -PreContent "<h2>Partition Information</h2>"
+
+
+
+$css = @"
+<style>
+h1 {
+
+     font-family: Arial, Helvetica, sans-serif;
+     color: #e68a00;
+     font-size: 28px;
+
+ }
+
+ 
+ h2 {
+
+     font-family: Arial, Helvetica, sans-serif;
+     color: #000099;
+     font-size: 16px;
+
+ }
+
+ 
+ 
+table {
+       font-size: 12px;
+       border: 0px; 
+       font-family: Arial, Helvetica, sans-serif;
+  } 
+  
+ td {
+       padding: 4px;
+       margin: 0px;
+       border: 0;
+  }
+  
+ th {
+     background: #395870;
+     background: linear-gradient(#49708f, #293f50);
+     color: #fff;
+     font-size: 11px;
+     text-transform: uppercase;
+     padding: 10px 15px;
+     vertical-align: middle;
+  }
+
+ tbody tr:nth-child(even) {
+     background: #f0f0f2;
+ }
+
+     #CreationDate {
+
+     font-family: Arial, Helvetica, sans-serif;
+     color: #ff3300;
+     font-size: 12px;
+
+ }
+</style>
+"@
+
+<#
+$css = @"
+<style>
+
+
+</style>
+"@
+#>
+
+$Report = ConvertTo-Html -Body "$RPSystem $RPOS $RPCPU $RPMemory $RPDisk $RPVolume $RPPartition" `
+     -Title "Computer Information" -Head $css -PostContent "<p>Creation Date: $(Get-Date)<p>"
+
+
+$Report | Out-File -FilePath .\report.html
