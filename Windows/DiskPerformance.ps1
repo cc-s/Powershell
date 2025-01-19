875c21b78b5a537cb5c296bb981d7021bba702bd
CC <ccs.135@gmail.com>
2025-01-19 13:17:42 +0100

    Updated Get-IOPS param settings

--- Windows/DiskPerformance.ps1
+++ Windows/DiskPerformance.ps1
@@ -3,22 +3,75 @@
 
 #Diskperformace Guide
 <##>
-IO Disk Transfers/sec (Disk Reads/sec, Disk Writes/sec)
+IO Disk Transfers/sec (Get-Disk Reads/sec, Disk Writes/sec)
 
 Latancy Avg. Disk sec/Transfer (Avg. Disk sec/Read, Avg. Disk sec/Write) 
 
 IO Size Avg. Disk Bytes/Transfer (Avg. Disk Bytes/Read, Avg. Disk Bytes/Write) 
 
-Throughput Disk Bytes/sec (Disk Read Bytes/sec, Disk Write Bytes/sec) 
+Throughput Disk Bytes/sec (Get-Disk Read Bytes/sec, Disk Write Bytes/sec) 
 #>
 
 #List  counters
- Get-Counter -ListSet * | select -ExpandProperty paths | select-string -Pattern 'Logisk' 
-
- 
+Get-Counter -ListSet * | Select-Object -ExpandProperty paths | Select-String -Pattern 'Logisk' 
 
 
-function Get-IOPS{
+function Get-IOPS {
+  [CmdletBinding()]
+  param
+  (
+    [Parameter(Mandatory = $true, 
+      ValueFromPipeline = $true,
+      ValueFromPipelineByPropertyName = $true, 
+      ValueFromRemainingArguments = $false, 
+      Position = 0,
+      ParameterSetName = 'Param set 1')]
+    [string]
+    $Drive,
+
+    [Parameter(Mandatory = $true, 
+      ValueFromPipeline = $true,
+      ValueFromPipelineByPropertyName = $true, 
+      ValueFromRemainingArguments = $false, 
+      Position = 1,
+      ParameterSetName = 'Param set 1')]
+    [int]
+    $SampleInterval,
+
+    [Parameter(Mandatory = $true, 
+      ValueFromPipeline = $true,
+      ValueFromPipelineByPropertyName = $true, 
+      ValueFromRemainingArguments = $false, 
+      Position = 2,
+      ParameterSetName = 'Param set 1')]
+    [int]
+    $MaxSamples
+  )
+  $Obj = @()
+  #Swedish
+  #$IOs = Get-Counter -Counter "\Logisk disk($Drive)\Disköverföringar/s" -SampleInterval $SampleInterval -MaxSamples $MaxSamples | select -ExpandProperty CounterSamples
+  
+  $IOs = Get-Counter -Counter "\LogicalDisk($Drive)\Disk Transfers/sec" -SampleInterval $SampleInterval -MaxSamples $MaxSamples | Select-Object -ExpandProperty CounterSamples
+  
+  foreach ($value in $IOs) {
+
+    $Props = [ordered]@{
+      Disk      = $value.InstanceName
+      IOPS      = $value.CookedValue -as [int]
+      Timestamp = $value.Timestamp
+
+    }
+
+    $Obj += New-Object -TypeName psobject -Property $Props
+  }
+
+  $Obj | Sort-Object -Property disk
+
+} #End Function IOPS
+
+Get-IOPS -Drive C: -SampleInterval 5 -MaxSamples 2
+
+function Get-Latency {
 
 
   [CmdletBinding()]
@@ -32,63 +85,27 @@ function Get-IOPS{
   )
   $Obj = @()
   #Swedish
-  #$IOs = Get-Counter -Counter "\Logisk disk($Drive)\Disköverföringar/s" -SampleInterval $SampleInterval -MaxSamples $MaxSamples | select -ExpandProperty CounterSamples
-  
-  $IOs = Get-Counter -Counter "\LogicalDisk($Drive)\Disk Transfers/sec" -SampleInterval $SampleInterval -MaxSamples $MaxSamples | select -ExpandProperty CounterSamples
-  
-  foreach ($value in $IOs){
+  #$Counters = Get-Counter -Counter "\Logisk disk($Drive)\Medel s/disköverföring" -SampleInterval $SampleInterval -MaxSamples $MaxSamples | select -ExpandProperty CounterSamples
 
-        $Props = [ordered]@{
-                Disk = $value.InstanceName
-                IOPS = $value.CookedValue -as [int]
-                Timestamp = $value.Timestamp
+  $Counters = Get-Counter -Counter "\LogicalDisk($Drive)\Avg. Disk sec/Transfer" -SampleInterval $SampleInterval -MaxSamples $MaxSamples | Select-Object -ExpandProperty CounterSamples
 
-        }
-
-    $Obj += New-Object -TypeName psobject -Property $Props
-    }
-
-$Obj | sort -Property disk
-
-} #End Function IOPS
-
-
-
-function Get-Latency{
-
-
-  [CmdletBinding()]
-  param
-  (
-    $Drive,
-
-    $SampleInterval,
-
-    $MaxSamples
-  )
-$Obj = @()
-#Swedish
-#$Counters = Get-Counter -Counter "\Logisk disk($Drive)\Medel s/disköverföring" -SampleInterval $SampleInterval -MaxSamples $MaxSamples | select -ExpandProperty CounterSamples
-
-$Counters = Get-Counter -Counter "\LogicalDisk($Drive)\Avg. Disk sec/Transfer" -SampleInterval $SampleInterval -MaxSamples $MaxSamples | select -ExpandProperty CounterSamples
-
-foreach ($value in $Counters){
+  foreach ($value in $Counters) {
 
     $Props = [ordered]@{
-            Disk = $value.InstanceName
-            Latency = $value.CookedValue -as [int]
-            Timestamp = $value.Timestamp
+      Disk      = $value.InstanceName
+      Latency   = $value.CookedValue -as [int]
+      Timestamp = $value.Timestamp
 
     }
 
-$Obj += New-Object -TypeName psobject -Property $Props
-}
+    $Obj += New-Object -TypeName psobject -Property $Props
+  }
 
-$Obj | sort disk
+  $Obj | Sort-Object disk
 
 } #End Function Latency
 
-function Get-DiskPerformance{
+function Get-DiskPerformance {
 
   [CmdletBinding()]
   param
@@ -104,29 +121,29 @@ function Get-DiskPerformance{
   #Swedish
   #$Counters = Get-Counter -Counter "\Logisk disk($Drive)\Medel s/disköverföring" -SampleInterval $SampleInterval -MaxSamples $MaxSamples | select -ExpandProperty CounterSamples
 
-  $Counters = Get-Counter -Counter "\LogicalDisk($Drive)\Disk Bytes/sec" -SampleInterval $SampleInterval -MaxSamples $MaxSamples | select -ExpandProperty CounterSamples
+  $Counters = Get-Counter -Counter "\LogicalDisk($Drive)\Disk Bytes/sec" -SampleInterval $SampleInterval -MaxSamples $MaxSamples | Select-Object -ExpandProperty CounterSamples
 
-  foreach ($value in $Counters){
+  foreach ($value in $Counters) {
 
     $Props = [ordered]@{
-            Disk = $value.InstanceName
-            Throughput = $value.CookedValue -as [int]
-            Timestamp = $value.Timestamp
+      Disk       = $value.InstanceName
+      Throughput = $value.CookedValue -as [int]
+      Timestamp  = $value.Timestamp
     }
 
     $Obj += New-Object -TypeName psobject -Property $Props
   }
 
-  $Obj | sort disk
+  $Obj | Sort-Object disk
 
 } #End Function DiskPerformance
 
 
- #IO
+#IO
 
- $Drive = 'C:'
- $SampleInterval = 2
- $MaxSamples = 2
+$Drive = 'C:'
+$SampleInterval = 2
+$MaxSamples = 2
 
 $IOps = Get-IOPS -Drive $Drive -SampleInterval $SampleInterval -MaxSamples $MaxSamples
 $Latency = Get-Latency -Drive $Drive -SampleInterval $SampleInterval -MaxSamples $MaxSamples
